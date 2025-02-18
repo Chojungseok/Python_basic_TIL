343c0034f638660eb158a27f00d59f7d1791ce03
chojungseok <whwndtjr9712@naver.com>
2025-02-18 14:25:25 +0900

    오후 중간 Commit

---
+++ fact.py
@@ -0,0 +1,15 @@
+# 팩토리얼 모율
+def factorial(n):
+    if n == 1:
+        return 1
+    else :
+        return  factorial(n-1) * n
+    
+
+
+
+def my_max(x, y):
+    if x > y:
+        return x
+    else:
+        return y
